61293c1fb6cdb65262659196ecffd577b9bc58ac
ShehrozIrfan <email@example.com>
2021-01-22 22:50:44 +0500

    added file 11

---
+++ 11. padStart() & padEnd()/app.js
@@ -0,0 +1,18 @@
+//padStart() & padEnd()
+
+// const example = 'John';
+
+// console.log(example.padStart(10, 'a'));
+// console.log(example.padEnd(5, ' '));
+// console.log(example.padStart(4, 1));
+// console.log(example.padEnd(6, 2));
+
+//Challenge
+
+let example = 'YouTube.com/CodingTutorials360';
+
+//in the statements below we didn't pass the second parameter.
+console.log(example.padStart(100));
+console.log(example.padStart(100).length);
+console.log(example.padEnd(1));
+console.log(example.padEnd(1).length);
